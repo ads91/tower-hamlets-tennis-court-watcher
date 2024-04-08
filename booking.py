742042d8d896a7a8551ed810ee1b530231c3c4b1
sanderson.ad@googlemail.com <5hp31d26>
2024-04-08 17:46:39 +0100

    Template for booking slots programatically.

--- booking.py
+++ booking.py
@@ -1,10 +1,8 @@
-import logging
 import selenium
-import datetime
 
 from main import *
 from data_io import load_file
-from utils.utils import get_chrome_driver, extract_site_contents
+from utils.utils import get_chrome_driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -17,11 +15,18 @@ AUTO_BOOK_DETAILS = load_file(
     "auto-book.json", io_type="os", prefix=os.path.join(os.path.dirname(__file__), ".secrets")
 )
 
-TEXT_INPUT_MAPPING = {
+PERSONAL_DETAILS_MAPPING = {
     "name": "#txt-name",
     "email": "#txt-email",
     "mobile": "#txt-mobile",
     "date-of-birth": "#txt-dob",
+}
+
+
+CARD_DETAILS_MAPPING = {
+    "card-number": "#root > form > div > div.CardField-input-wrapper > span.CardField-number.CardField-child > span:nth-child(2) > div > div.CardNumberField-input-wrapper > span > input",
+    "expiry": "#root > form > div > div.CardField-input-wrapper > span.CardField-restWrapper > span.CardField-expiry.CardField-child > span > span > input",
+    "cvc": "#root > form > div > div.CardField-input-wrapper > span.CardField-restWrapper > span.CardField-cvc.CardField-child > span > span > input"
 }
 
 
@@ -42,43 +47,71 @@ def _add_to_basket(driver, table_index, court, time, wait=1):
     return False
 
 
-def book(date, time, day_start=7, courts=(1, 2), wait=1):
-    logger.info(f"Checking for available slots on {date} at {time}")
+def book(date, time_, day_start=7, courts=(1, 2), wait=1, pay=False):
+    logger.info(f"Checking for available slots on {date} at {time_}")
 
-    time_string = str(time).zfill(2) + ":00"
+    time_string = str(time_).zfill(2) + ":00"
 
     url = BOOKING_WIDGET_URL.format(date=date.strftime(BOOKING_WIDGET_URL_DATE_FORMAT))
 
     # Spin up driver
     driver = get_chrome_driver(
-        url=url, driver_path="", sleep=wait
+        url=url, driver_path="", sleep=3
     )
 
     # Attempt to add a court to the basket
-    table_index = 1 + (time - day_start)
+    added_to_basket, table_index = True, 1 + (time_ - day_start)
     for court in list(courts):
         if _add_to_basket(driver, table_index, court, time=time_string, wait=wait):
+            added_to_basket = True
             break
+
+    # Don't continue execution if nothing added to basket
+    if not added_to_basket:
+        logger.warning("No slots were booked")
+        return
 
     # Navigate to the basket
     driver.get("https://tennistowerhamlets.com/basket")
+    time.sleep(3)
 
-    # Fill in required details for payment
-    for k, v in AUTO_BOOK_DETAILS.items():
-        selector = TEXT_INPUT_MAPPING[k]
+    # Fill in required personal details
+    for key, selector in PERSONAL_DETAILS_MAPPING.items():
         form = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
-        form.send_keys(v)
-        logger.info(f"Entered value for {k}")
+        form.send_keys(AUTO_BOOK_DETAILS[key])
+        logger.info(f"Entered value for {key}")
 
     # Set gender to "prefer not to say"
     selector = " #frm_basket_customer > div:nth-child(8) > fieldset > label:nth-child(4) > input[type=radio]"
     radio_button = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
     radio_button.click()
 
+    # Save & got to checkout
+    selector = "#frm_basket_customer > div.controls > button"
+    radio_button = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+    radio_button.click()
+
+    # Pay with card
+    selector = "#btn-pay-now"
+    radio_button = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+    radio_button.click()
+
+    # Fill in payment details
+    for key, selector in CARD_DETAILS_MAPPING.items():
+        form = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+        form.send_keys(AUTO_BOOK_DETAILS[key])
+        logger.info(f"Entered value for {key}")
+
+    # Press the pay button
+    if pay:
+        pass
+    else:
+        logging.warning("Not making payment until explicitly told to")
+
     print("")
 
 
 if __name__ == "__main__":
     book(
-        date=datetime.date(2024, 4, 9), time=15
+        date=datetime.date(2024, 4, 9), time_=15, wait=1, pay=False
     )
