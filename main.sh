2d84d0b41ecf43aa73af92898992e8049765aee1
ads91 <34964684+ads91@users.noreply.github.com>
2024-06-18 20:22:31 +0100

    Feature/shell script auto restart (#14)
    * Now auto-restarting the shell script running the watcher if/when the Python process exits.
    
    * Updating HTML parsing.

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
 cd /Users/andrewsanderson/Documents/dev/tower-hamlets-tennis-court-watcher
-#python3 -m pip install -r requirement.txt --upgrade
-python3 main.py
+
+# The command you want to run indefinitely
+COMMAND="./venv/bin/python3 main.py"
+
+# Loop indefinitely
+while true; do
+    # Run the command
+    $COMMAND
+    # Wait for a second before restarting (optional)
+    sleep 10
+done
